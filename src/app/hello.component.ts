bd63963c6dc91a44611eaf2bf60abdba9ee5d38f
coderbase-it <coderbaseit@gmail.com>
2022-03-15 17:07:46 +0100

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,6 +3,7 @@ import {
   Input,
   DoCheck,
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
 } from '@angular/core';
 
 @Component({
@@ -10,14 +11,25 @@ import {
   template: `
     <div>
       Hello {{person.name}} with
-      {{ person.age }} year old
+      {{ oldPersonAge }} year old
     </div>
   `,
   styles: [`:host { font-family: Lato; } div {  border: 1px solid black; } `],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HelloComponent {
+export class HelloComponent implements DoCheck {
   @Input() person: Person;
+  oldPersonAge: number;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngDoCheck(): void {
+    console.log(this.person);
+    if (this.person.age !== this.oldPersonAge) {
+      this.oldPersonAge = this.person.age;
+      this.cd.markForCheck();
+    }
+  }
 }
 export interface Person {
   name: string;
